59f81b5dedc25c9b464a7ab0083f6d9bdb237e24
Brennan McFarland <brennanfrancismcfarland@gmail.com>
2020-02-23 21:04:23 -0500

    upticked dependency version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 cd .. || exit
 cd ./arctic_flaming_monkey_typhoon || exit
 git submodule update --init --recursive
-git checkout v0.1.0 # change this to update dependency version referenced
+git checkout v0.2.0 # change this to update dependency version referenced
